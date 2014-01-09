41c77d92452c138c4aec8fe3190431aa6b525659
odanielson <olov.danielson@gmail.com>
2014-01-10 00:06:43 +0100

    Send events for dirs to...

--- scandir.go
+++ scandir.go
@@ -13,6 +13,9 @@ func ScanDir(a_path string, a_fileMsgs chan FileMsg,
     if files, err := ioutil.ReadDir(a_path); err == nil {
         for _, file := range files {
             if file.IsDir() {
+                a_fileMsgs <- FileMsg{a_path, file.Name(), CREATED}
+                a_events <- Event{DEBUG, fmt.Sprintf("Found dir %s",
+                    path.Join(a_path, file.Name())), nil}
                 ScanDir(path.Join(a_path, file.Name()), a_fileMsgs, a_events)
             } else {
                 a_fileMsgs <- FileMsg{a_path, file.Name(), CREATED}
